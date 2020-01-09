d8d00b056107e467c41e317af22b4f8794fa693e
Rasa Jakubonyte <rasa@Nathans-MacBook-Pro.local>
2020-01-08 19:31:46 -0600

    second commit

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,20 +1,87 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.Objects;
+
 public class Job {
 
     private int id;
     private static int nextId = 1;
-
     private String name;
+    public Job(){
+        id=nextId;
+        nextId++;
+    }
+    public Job(String aName, String employer, String location, String positionType, String coreCompetency ){
+        this();
+        this.name = aName;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
 
+
+    }
     public int getId() {
         return id;
     }
 
-    private Employer employer;
-    private Location location;
-    private PositionType positionType;
-    private CoreCompetency coreCompetency;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(String positionType) {
+        this.positionType = positionType;
+    }
+
+    public String getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(String coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    private String employer;
+    private String location;
+    private String positionType;
+    private String coreCompetency;
 
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
